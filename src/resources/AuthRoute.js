ccf3874b59b9025351d376ce28673c5080865b18
Dawid <wizard91@o2.pl>
2020-03-13 21:57:35 +0000

    setting firebase with redux in progress

--- src/resources/AuthRoute.js
+++ src/resources/AuthRoute.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-const AuthRoute = ({component: Component, authenticated, ...rest  })=> (
+const AuthRoute = ({ component: Component, authenticated, ...rest })=> (
    <Route {...rest}
    render={(props) => authenticated === true ? <Redirect to='/' /> : <Component {...props} />}
    />
