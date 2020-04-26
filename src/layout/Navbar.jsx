7db1f9b8798f3fabd94370721d5443d97576a7e1
silus91 <wizard91@o2.pl>
2020-04-26 22:10:16 +0100

    removed all not nesesery imgs from folder

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -4,8 +4,8 @@ import { connect } from 'react-redux';
 import { logout } from '../actions/authActions';
 import M from "materialize-css";
 import './Navbar.css';
-import profile from '../resources/imgs/profile.png';
-import silverHand from '../resources/imgs/silverHandMini.jpg';
+
+const profilePic = "https://firebasestorage.googleapis.com/v0/b/cdred-project.appspot.com/o/pageImgs%2Fprofile.png?alt=media&token=12756539-29a9-4177-bb3b-c81548f4acbd";
 
 class Navbar extends Component {
 
@@ -45,10 +45,13 @@ class Navbar extends Component {
           <li>
             <div className="user-view">
               <div className="background">
-                <img alt="" src={silverHand} />
+                <img
+                  alt="Johny Silver Hand" 
+                  src="https://firebasestorage.googleapis.com/v0/b/cdred-project.appspot.com/o/pageImgs%2FsilverHandMini.jpg?alt=media&token=ed5861f8-a999-490d-840c-86bd4cda1757"
+                />
               </div>
-              <a href="#user"><img alt="" className="circle" src={authenticated ? credentials.photoURL : profile} /></a>
-              <a href="#name"><span className="white-text name">{authenticated ? credentials.firstName : "Guest"}</span></a>
+              <a href="#user"><img alt="" className="circle" src={authenticated ? credentials.photoURL : profilePic} /></a>
+              <a href="#name"><span className="red-text name">{authenticated ? credentials.firstName : "Guest"}</span></a>
             </div>
           </li>
           <li><Link to='/'>Home</Link></li>
