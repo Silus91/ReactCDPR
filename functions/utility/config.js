aaeb3784afa00f14e96bfb19ffd1dbdbef1717a4
Silus91 <41906032+Silus91@users.noreply.github.com>
2020-04-27 10:43:24 +0100

    removing config.js

--- functions/utility/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  apiKey: "AIzaSyDQNsDHwcQZdZ_9EqwFODmjPJw33GgO-1A",
-  authDomain: "cdred-project.firebaseapp.com",
-  databaseURL: "https://cdred-project.firebaseio.com",
-  projectId: "cdred-project",
-  storageBucket: "cdred-project.appspot.com",
-  messagingSenderId: "536453525950",
-  appId: "1:536453525950:web:59c0bf958d495e74a889ef"
-};
