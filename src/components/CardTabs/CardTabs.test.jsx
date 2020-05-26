74e8c161dc3d9329e6ee01cf640233e28844f21c
silus91 <wizard91@o2.pl>
2020-05-26 22:19:27 +0100

    tests with jest and enzyme setup

---
+++ src/components/CardTabs/CardTabs.test.jsx
@@ -0,0 +1,18 @@
+import { mount } from "enzyme";
+import CardTabs from "./CardTabs";
+
+describe("CardTabs component", () => {
+  // it should render (bez zadnego errror itp)
+
+  // it should render tabs
+
+  // it should render containers
+
+  it("should show c", () => {
+    const CardTabsWrapper = mount(<CardTabs />);
+
+    /// CardTabsWrapper
+
+    expect(1).toEqual(1);
+  });
+});
